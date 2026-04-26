f7326e96214a6adb339b3a29ab095fd3bf3fb808
sunqi <zqbysq@163.com>
2026-04-26 20:56:10 +0800

    update reinit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + TypeScript + Vite
+
+- React 19.2.5
+- three 0.184.0
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import SimpleCube from "./SimpleCube";
+import "./App.css";
+
+function App() {
+  return <SimpleCube />;
+}
+
+export default App;
